68bb706f9b4cbe9414a3d7f35596c956088c64f2
kwonsoryeong <pjh9438@naver.com>
2021-03-17 16:08:17 +0900

    0317

--- src/pages/quote/Quote.js
+++ src/pages/quote/Quote.js
@@ -11,24 +11,25 @@ class Quote extends Component {
     };
     componentDidMount(){
         if(this.state.additionalEmployment1 > 0 && this.state.additionalEmployment2 > 0){
-            if(this.state.proA && !this.state.proB && !this.state.proC){ //A
-                if(this.state.additionalEmployment2 < 2000000){
-                    this.setState({maxSupport: (this.state.additionalEmployment2*0.9+100000)*6*this.state.additionalEmployment1})
-                }else{
-                    this.setState({maxSupport: this.state.additionalEmployment1*1900000*6})
-                }
-                this.setState({ 
-                    totalMonthPay:this.state.additionalEmployment1*this.state.additionalEmployment2,
-                    monthExpense :this.state.additionalEmployment1*150000 + 150000,
-                    totalExpense :(this.state.additionalEmployment1*150000+150000)*6,
-                    maxCusProfit : this.state.maxSupport - this.state.totalExpense,
-                })
-                //this.setState({maxSupport: })
-            }else if(!this.state.proA && this.state.proB && !this.state.proC){ //B
-                this.setState({maxSupport: this.state.additionalEmployment1*27000000})
-            }else if(!this.state.proA && !this.state.proB && this.state.proC){ //C
-                this.setState({maxSupport: this.state.additionalEmployment1*50000*36})
-            }else if(this.state.proA && this.state.proB && !this.state.proC){ //AB
+            //pro A 최대 지원금
+            if(this.state.additionalEmployment2 < 200){
+                this.setState({proA: (this.state.additionalEmployment2*0.9+10)*6 * this.state.additionalEmployment1})
+            }else{
+                this.setState({proA: 190*6 * this.state.additionalEmployment1})
+            }
+            //pro B 최대 지원금
+            this.setState({proB: this.state.additionalEmployment1*75*30})
+            //pro C 최대 지원금
+            this.setState({proC: this.state.additionalEmployment1*8*30})
+
+            /*this.setState({ 
+                totalMonthPay:this.state.additionalEmployment1*this.state.additionalEmployment2,
+                monthExpense :this.state.additionalEmployment1*150000 + 150000,
+                totalExpense :(this.state.additionalEmployment1*150000+150000)*6,
+                maxCusProfit : this.state.maxSupport - this.state.totalExpense,
+            })*/
+            
+            /*}else if(this.state.proA && this.state.proB && !this.state.proC){ //AB
                 if(this.state.additionalEmployment2 < 2000000){
                     this.setState({maxSupport: (this.state.additionalEmployment2*0.9 +100000) *6 * this.state.additionalEmployment1 + (this.state.additionalEmployment1*22500000)})
                 }else{
@@ -48,15 +49,14 @@ class Quote extends Component {
                 }else{
                     this.setState({maxSupport: this.state.additionalEmployment1*1900000 *6 + (this.state.additionalEmployment1*22500000) + (this.state.additionalEmployment1*50000 * 36)})
                 }
-            }
-            if(this.state.proB || this.state.proC){
-                this.setState({ 
-                    totalMonthPay:this.state.additionalEmployment1*this.state.additionalEmployment2,
-                    monthExpense :this.state.additionalEmployment1*150000 + 150000,
-                    totalExpense :(this.state.additionalEmployment1*150000+150000)*36,
-                    maxCusProfit : this.state.maxSupport - this.state.totalExpense,
-                })
-            }
+            }*/
+            
+            this.setState({ 
+                totalMonthPay:this.state.additionalEmployment1*this.state.additionalEmployment2,
+                monthExpense :this.state.additionalEmployment1*15 + 15,
+                totalExpense :(this.state.additionalEmployment1*15+15)*36,
+            })
+            
 
         }
     }
@@ -71,18 +71,36 @@ class Quote extends Component {
             <form>
                 <hr/>
                 <label>기업명 : {this.state.companyName}</label><br />
-                <label>고객 사업자/법인등록번호 : </label><br /><br/>
-                <label>{this.state.businessType1==true?"개인사업자":this.state.businessType2===true?"법인사업자":this.state.businessType3===true?"성장유망업종":""}</label><br/>
+                {/*<label>고객 사업자/법인등록번호 : </label><br /><br/>*/}
+                <label>{this.state.radioGroup.businessType1==true?"개인사업자":this.state.radioGroup.businessType2===true?"법인사업자":""}</label><br/><br/>
+                
+                <label>업태 : {this.state.status}</label><br/>
+                <label>업종 : {this.state.type}</label><br/>
+                <label>성장유망업종 : {this.state.businessType3?"O":"X"}</label><br/><br/>
+                
                 <label>기존 직원 정보</label><br />
                 <label>4대보험 가입 직원 수 : {this.state.subscribers}명</label><br />
                 <label>4대보험 미가입 직원 수 : {this.state.unsubscribers}명</label><br />
                 <label>추가 고용 계획 : {this.state.additionalEmployment1}명</label><br /><br/>
                 <label>간이 견적표</label><br />
-                <label>총 월급여액 : {this.state.totalMonthPay}원</label><br/>
-                <label>월 비용 : {this.state.monthExpense}원</label><br/>
-                <label>총 비용 : {this.state.totalExpense}원</label><br/>
-                <label>최대지원금액 : {this.state.maxSupport}원</label><br/>
-                <label>최대고객이익 : {this.state.maxSupport - this.state.totalExpense}원</label><br/><br/>
+
+                {/*<label>총 월급여액 : {this.state.totalMonthPay}원</label><br/>*/}
+                <label>월 비용 : {this.state.monthExpense}만 원</label><br/>
+                
+                <label>proA</label><br />
+                <label>총 비용 : {this.state.monthExpense * 6}만 원</label><br/>
+                <label>최대지원금액 : {this.state.proA}만 원</label><br/>
+                <label>최대고객이익 : {this.state.proA - (this.state.totalMonthPay+this.state.monthExpense) * 36}만 원</label><br/>
+
+                <label>proB</label><br />
+                <label>총 비용 : {this.state.monthExpense * 30}만 원</label><br/>
+                <label>최대지원금액 : {this.state.proB}만 원</label><br/>
+                <label>최대고객이익 : {this.state.proB - (this.state.totalMonthPay+this.state.monthExpense) * 36}만 원</label><br/>
+
+                <label>proC</label><br />
+                <label>총 비용 : {this.state.monthExpense *30}만 원</label><br/>
+                <label>최대지원금액 : {this.state.proC}만 원</label><br/>
+                <label>최대고객이익 : {this.state.proC - (this.state.totalMonthPay+this.state.monthExpense)*36}만 원</label><br/><br/>
 
                 <label>위 견적은 간이 견적으로 실제 견적과 차이가 있을 수 있습니다.</label><br/><br/>
 
